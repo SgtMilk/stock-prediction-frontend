54ee61424846400025350d8e352211d9e321f7bd
SgtMilk <alix.routhier-lalonde@hotmail.com>
2021-08-07 14:17:39 -0400

    forgot some files

---
+++ .storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "stories": [
+    "../globalComponents/**/*.stories.@(js|jsx|ts|tsx)",
+    "../pages/**/*.stories.@(js|jsx|ts|tsx)"
+],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
